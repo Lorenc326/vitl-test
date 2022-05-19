8723aa707e059cff1fe204cb64589b967ed28835
Lorenc326 <iLorenc326@gmail.com>
2022-05-19 21:50:26 +0300

    Add validator

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/Lorenc326/vitl-test
 go 1.16
 
 require (
+	github.com/go-playground/validator/v10 v10.11.0
 	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/labstack/echo/v4 v4.7.2
 )
--- number/handler.go
+++ number/handler.go
@@ -1,14 +1,16 @@
 package number
 
 import (
+	"github.com/Lorenc326/vitl-test/services/validator"
 	"github.com/labstack/echo/v4"
 	"log"
+	"net/http"
 )
 
 type GenerateInput struct {
-	FromNumber   int `json:"from_number"`
-	ToNumber     int `json:"to_number"`
-	TotalNumbers int `json:"total_numbers"`
+	FromNumber   int `json:"from_number" validate:"required"`
+	ToNumber     int `json:"to_number" validate:"required"`
+	TotalNumbers int `json:"total_numbers" validate:"required"`
 }
 
 func Generate(c echo.Context) error {
@@ -24,6 +26,10 @@ func Generate(c echo.Context) error {
 	if err := c.Bind(&input); err != nil {
 		log.Println(err)
 		return echo.ErrBadRequest
+	}
+	if err := validator.Validate.Struct(input); err != nil {
+		log.Println(err)
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
 
 	counts, err := countsForRandomSequence(sort, &input)
